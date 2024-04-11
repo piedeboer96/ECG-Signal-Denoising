e55fad4dbcc713a5ad1a08092fd9c7c795710127
piedeboer96 <piedeboer96@gmail.com>
2024-04-11 11:51:47 +0200

    morning work

--- src/denoise_emd.py
+++ src/denoise_emd.py
@@ -1,26 +1,30 @@
-import emd
+# import emd
 import wfdb
+import sys
 import numpy as np
 from scipy import signal, ndimage
 import matplotlib.pyplot as plt
 
+print('Script is running... Zzzzz')
+print('Virtual Environment:', sys.prefix)
+
 # Load ECG signal and annotation
-record = wfdb.rdsamp('/Users/piedeboer/Desktop/Thesis/data/nstdb/118e06', sampto=3000)
+# record = wfdb.rdsamp('/Users/piedeboer/Desktop/Thesis/data/nstdb/118e06', sampto=3000)
 
-# Extract Lead I ECG signal
-noisy_signal = record[0][:, 0]
+# # Extract Lead I ECG signal
+# noisy_signal = record[0][:, 0]
 
-# EMD
-denoised_signal =  emd.sift.sift(noisy_signal)
+# # EMD
+# denoised_signal =  emd.sift.sift(noisy_signal)
 
-# Example of plotting the original and denoised signals
-import matplotlib.pyplot as plt
+# # Example of plotting the original and denoised signals
+# import matplotlib.pyplot as plt
 
-plt.figure(figsize=(10, 6))
-plt.plot(noisy_signal, label='Noisy Signal')
-plt.plot(denoised_signal, label='Denoised Signal')
-plt.xlabel('Sample')
-plt.ylabel('Amplitude')
-plt.title('Original vs Denoised Signal')
-plt.legend()
-plt.show()
+# plt.figure(figsize=(10, 6))
+# plt.plot(noisy_signal, label='Noisy Signal')
+# plt.plot(denoised_signal, label='Denoised Signal')
+# plt.xlabel('Sample')
+# plt.ylabel('Amplitude')
+# plt.title('Original vs Denoised Signal')
+# plt.legend()
+# plt.show()
