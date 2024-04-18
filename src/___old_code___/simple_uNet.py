d220562774b0557f43e24c5c4f90c780f5a7e22b
piedeboer96 <piedeboer96@gmail.com>
2024-04-18 15:06:30 +0200

    messy implementation

---
+++ src/___old_code___/simple_uNet.py
@@ -0,0 +1,128 @@
+import math
+import torch
+from torch import nn
+from inspect import isfunction
+
+class MyBlock(nn.Module):
+    def __init__(self, shape, in_c, out_c, kernel_size=3, stride=1, padding=1, activation=None, normalize=True):
+        super(MyBlock, self).__init__()
+        self.ln = nn.LayerNorm(shape)
+        self.conv1 = nn.Conv2d(in_c, out_c, kernel_size, stride, padding)
+        self.conv2 = nn.Conv2d(out_c, out_c, kernel_size, stride, padding)
+        self.activation = nn.SiLU() if activation is None else activation
+        self.normalize = normalize
+
+    def forward(self, x):
+        out = self.ln(x) if self.normalize else x
+        out = self.conv1(out)
+        out = self.activation(out)
+        out = self.conv2(out)
+        out = self.activation(out)
+        return out
+
+# Allow to do noiselevelembedding (condition on noise a_bar)
+class NoiseLevelEmbedding(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, noise_level):
+        count = self.dim // 2
+        step = torch.arange(count, dtype=noise_level.dtype, device=noise_level.device) / count
+        encoding = noise_level.unsqueeze(1) * torch.exp(-math.log(1e4) * step.unsqueeze(0))
+        encoding = torch.cat([torch.sin(encoding), torch.cos(encoding)], dim=-1)
+        return encoding
+
+class MyUNet(nn.Module):
+    def __init__(self, noise_level_emb_dim=2):
+        super(MyUNet, self).__init__()
+        print('U Net Time...')
+
+        # Noise level embedding
+        self.noise_level_embed = NoiseLevelEmbedding(noise_level_emb_dim)
+        
+        # First half
+        self.b1 = nn.Sequential(
+            MyBlock((1, 28, 28), 1, 10),
+            MyBlock((10, 28, 28), 10, 10),
+            MyBlock((10, 28, 28), 10, 10)
+        )
+        self.down1 = nn.Conv2d(10, 10, 4, 2, 1)
+
+        self.b2 = nn.Sequential(
+            MyBlock((10, 14, 14), 10, 20),
+            MyBlock((20, 14, 14), 20, 20),
+            MyBlock((20, 14, 14), 20, 20)
+        )
+        self.down2 = nn.Conv2d(20, 20, 4, 2, 1)
+
+        self.b3 = nn.Sequential(
+            MyBlock((20, 7, 7), 20, 40),
+            MyBlock((40, 7, 7), 40, 40),
+            MyBlock((40, 7, 7), 40, 40)
+        )
+        self.down3 = nn.Sequential(
+            nn.Conv2d(40, 40, 2, 1),
+            nn.SiLU(),
+            nn.Conv2d(40, 40, 4, 2, 1)
+        )
+
+        # Bottleneck
+        self.b_mid = nn.Sequential(
+            MyBlock((40, 3, 3), 40, 20),
+            MyBlock((20, 3, 3), 20, 20),
+            MyBlock((20, 3, 3), 20, 40)
+        )
+
+        # Second half
+        self.up1 = nn.Sequential(
+            nn.ConvTranspose2d(40, 40, 4, 2, 1),
+            nn.SiLU(),
+            nn.ConvTranspose2d(40, 40, 2, 1)
+        )
+
+        self.b4 = nn.Sequential(
+            MyBlock((80, 7, 7), 80, 40),
+            MyBlock((40, 7, 7), 40, 20),
+            MyBlock((20, 7, 7), 20, 20)
+        )
+
+        self.up2 = nn.ConvTranspose2d(20, 20, 4, 2, 1)
+        self.b5 = nn.Sequential(
+            MyBlock((40, 14, 14), 40, 20),
+            MyBlock((20, 14, 14), 20, 10),
+            MyBlock((10, 14, 14), 10, 10)
+        )
+
+        self.up3 = nn.ConvTranspose2d(10, 10, 4, 2, 1)
+        self.b_out = nn.Sequential(
+            MyBlock((20, 28, 28), 20, 10),
+            MyBlock((10, 28, 28), 10, 10),
+            MyBlock((10, 28, 28), 10, 10, normalize=False)
+        )
+
+        self.conv_out = nn.Conv2d(10, 1, 3, 1, 1)
+
+    def forward(self, x, noise_level):
+        print('U Net Forward call...')
+        n = len(x)
+
+        print('noise leve')
+        print(noise_level.shape)
+        print('x')
+        print(x.shape)
+
+        noise_level_embedded = self.noise_level_embed(noise_level).unsqueeze(-1).unsqueeze(-1)  # Reshape noise level embedding
+        out1 = self.b1(x + noise_level_embedded)  # (N, 10, 28, 28)
+        out2 = self.b2(self.down1(out1) + noise_level_embedded)  # (N, 20, 14, 14)
+        out3 = self.b3(self.down2(out2) + noise_level_embedded)  # (N, 40, 7, 7)
+        out_mid = self.b_mid(self.down3(out3) + noise_level_embedded)  # (N, 40, 3, 3)
+        out4 = torch.cat((out3, self.up1(out_mid)), dim=1)  # (N, 80, 7, 7)
+        out4 = self.b4(out4 + noise_level_embedded)  # (N, 20, 7, 7)
+        out5 = torch.cat((out2, self.up2(out4)), dim=1)  # (N, 40, 14, 14)
+        out5 = self.b5(out5 + noise_level_embedded)  # (N, 10, 14, 14)
+        out = torch.cat((out1, self.up3(out5)), dim=1)  # (N, 20, 28, 28)
+        out = self.b_out(out + noise_level_embedded)  # (N, 1, 28, 28)
+        out = self.conv_out(out)
+        return out
+
---
+++ src/cond_model/model_SR3.py
@@ -0,0 +1,249 @@
+import math
+import torch
+from torch import device, nn, einsum
+import torch.nn.functional as F
+from inspect import isfunction
+from functools import partial
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+
+############################################################
+# Utility Crap
+def visualize_image(image):
+    # Extract the last two dimensions
+    if image.ndim == 3:
+        image = image[0]  # Assuming only one image is present
+
+    plt.imshow(image, cmap='gray')
+    plt.title('Image')
+    plt.axis('off')
+    plt.show()
+############################################################
+
+
+# TAKEN FROM ECG
+
+
+# gaussian diffusion trainer class
+
+def exists(x):
+    return x is not None
+
+
+def default(val, d):
+    if exists(val):
+        return val
+    return d() if isfunction(d) else d
+
+
+class GaussianDiffusion(nn.Module):
+    def __init__(
+        self,
+        denoise_fn,
+        image_size,
+        channels=3,         #  (e.g. RGB)
+        loss_type='l1',
+        conditional=True,
+        config_diff=None
+    ):
+        super().__init__()
+        self.channels = channels
+        self.image_size = image_size
+        self.denoise_fn = denoise_fn
+        self.loss_type = loss_type
+        self.conditional = conditional
+        self.num_timesteps = config_diff['num_steps']
+        self.set_loss(device=torch.device("cpu"))
+        self.set_new_noise_schedule(config_diff, device=torch.device("cpu"))
+
+
+# ******************************************
+# ******************************************
+# THESE METHODS ARE NOT CHANGED 
+
+    # ********************************
+    # Loss Function
+    def set_loss(self, device):
+        if self.loss_type == 'l1':
+            self.loss_func = nn.L1Loss(reduction='sum').to(device)
+        elif self.loss_type == 'l2':
+            self.loss_func = nn.MSELoss(reduction='sum').to(device)
+        else:
+            raise NotImplementedError()
+
+    # ********************************
+    # Noise Schedule
+    def make_beta_schedule(self, schedule='linear', n_timesteps=1000, start=1e-5, end=1e-2):
+        if schedule == 'linear':
+            betas = torch.linspace(start, end, n_timesteps)
+        elif schedule == "quad":
+            betas = torch.linspace(start ** 0.5, end ** 0.5, n_timesteps) ** 2
+        elif schedule == "sigmoid":
+            betas = torch.linspace(-6, 6, n_timesteps)
+            betas = torch.sigmoid(betas) * (end - start) + start
+        return betas
+
+    def set_new_noise_schedule(self, config_diff, device):
+        
+        to_torch = partial(torch.tensor, dtype=torch.float32, device=device)
+        
+        betas = self.make_beta_schedule(schedule=config_diff["schedule"], n_timesteps=config_diff["num_steps"],
+                                            start=config_diff["beta_start"], end=config_diff["beta_end"])
+        betas = betas.detach().cpu().numpy() if isinstance(
+            betas, torch.Tensor) else betas
+        
+        # Core computations...
+        alphas = 1. - betas
+        alphas_cumprod = np.cumprod(alphas, axis=0)
+        alphas_cumprod_prev = np.append(1., alphas_cumprod[:-1])
+        self.sqrt_alphas_cumprod_prev = np.sqrt(
+            np.append(1., alphas_cumprod))
+        
+        # Work with register buffers...
+        self.register_buffer('betas', to_torch(betas))
+        self.register_buffer('alphas_cumprod', to_torch(alphas_cumprod))
+        self.register_buffer('alphas_cumprod_prev',
+                             to_torch(alphas_cumprod_prev))
+
+        # calculations for diffusion q(x_t | x_{t-1}) and others
+        self.register_buffer('sqrt_alphas_cumprod',
+                             to_torch(np.sqrt(alphas_cumprod)))
+        self.register_buffer('sqrt_one_minus_alphas_cumprod',
+                             to_torch(np.sqrt(1. - alphas_cumprod)))
+        self.register_buffer('log_one_minus_alphas_cumprod',
+                             to_torch(np.log(1. - alphas_cumprod)))
+        self.register_buffer('sqrt_recip_alphas_cumprod',
+                             to_torch(np.sqrt(1. / alphas_cumprod)))
+        self.register_buffer('sqrt_recipm1_alphas_cumprod',
+                             to_torch(np.sqrt(1. / alphas_cumprod - 1)))
+        
+        # calculations for posterior q(x_{t-1} | x_t, x_0)
+        posterior_variance = betas * (1. - alphas_cumprod_prev) / (1. - alphas_cumprod)
+        # above: equal to 1. / (1. / (1. - alpha_cumprod_tm1) + alpha_t / beta_t)
+        self.register_buffer('posterior_variance',
+                             to_torch(posterior_variance))
+        # below: log calculation clipped because the posterior variance is 0 at the beginning of the diffusion chain
+        self.register_buffer('posterior_log_variance_clipped', to_torch(
+            np.log(np.maximum(posterior_variance, 1e-20))))
+        self.register_buffer('posterior_mean_coef1', to_torch(
+            betas * np.sqrt(alphas_cumprod_prev) / (1. - alphas_cumprod)))
+        self.register_buffer('posterior_mean_coef2', to_torch(
+            (1. - alphas_cumprod_prev) * np.sqrt(alphas) / (1. - alphas_cumprod)))
+    
+
+    # ********************************
+    # Sampling, diffusion, backwards,  ... 
+    def predict_start_from_noise(self, x_t, t, noise):
+        return self.sqrt_recip_alphas_cumprod[t] * x_t - \
+            self.sqrt_recipm1_alphas_cumprod[t] * noise
+
+    def q_posterior(self, x_start, x_t, t):
+        posterior_mean = self.posterior_mean_coef1[t] * \
+            x_start + self.posterior_mean_coef2[t] * x_t
+        posterior_log_variance_clipped = self.posterior_log_variance_clipped[t]
+        return posterior_mean, posterior_log_variance_clipped
+
+    def p_mean_variance(self, x, t, clip_denoised: bool, condition_x=None):
+        batch_size = x.shape[0]
+        noise_level = torch.FloatTensor(
+            [self.sqrt_alphas_cumprod_prev[t+1]]).repeat(batch_size, 1).to(x.device)
+        if condition_x is not None:
+            x_recon = self.predict_start_from_noise(
+                x, t=t, noise=self.denoise_fn(torch.cat([condition_x, x], dim=1), noise_level))
+        else:
+            x_recon = self.predict_start_from_noise(
+                x, t=t, noise=self.denoise_fn(x, noise_level))
+
+        if clip_denoised:
+            x_recon.clamp_(-1., 1.)
+
+        model_mean, posterior_log_variance = self.q_posterior(
+            x_start=x_recon, x_t=x, t=t)
+        return model_mean, posterior_log_variance
+
+    @torch.no_grad()
+    def p_sample(self, x, t, clip_denoised=True, condition_x=None):
+        model_mean, model_log_variance = self.p_mean_variance(
+            x=x, t=t, clip_denoised=clip_denoised, condition_x=condition_x)
+        noise = torch.randn_like(x) if t > 0 else torch.zeros_like(x)
+        return model_mean + noise * (0.5 * model_log_variance).exp()
+
+    @torch.no_grad()
+    def p_sample_loop(self, x_in, continous=False):
+        device = self.betas.device
+        sample_inter = (1 | (self.num_timesteps//10))
+        if not self.conditional:
+            shape = x_in
+            img = torch.randn(shape, device=device)
+            ret_img = img
+            for i in tqdm(reversed(range(0, self.num_timesteps)), desc='sampling loop time step', total=self.num_timesteps):
+                img = self.p_sample(img, i)
+                if i % sample_inter == 0:
+                    ret_img = torch.cat([ret_img, img], dim=0)
+        else:
+            x = x_in
+            shape = x.shape
+            img = torch.randn(shape, device=device)
+            ret_img = x
+            for i in tqdm(reversed(range(0, self.num_timesteps)), desc='sampling loop time step', total=self.num_timesteps):
+                img = self.p_sample(img, i, condition_x=x)
+                if i % sample_inter == 0:
+                    ret_img = torch.cat([ret_img, img], dim=0)
+        if continous:
+            return ret_img
+        else:
+            return ret_img[-1]
+
+    @torch.no_grad()
+    def sample(self, batch_size=1, continous=False):
+        image_size = self.image_size
+        channels = self.channels
+        return self.p_sample_loop((batch_size, channels, image_size, image_size), continous)
+
+    @torch.no_grad()
+    def super_resolution(self, x_in, continous=False):
+        return self.p_sample_loop(x_in, continous)
+
+    def q_sample(self, x_start, continuous_sqrt_alpha_cumprod, noise=None):
+        noise = default(noise, lambda: torch.randn_like(x_start))
+
+        # random gama
+        return (
+            continuous_sqrt_alpha_cumprod * x_start +
+            (1 - continuous_sqrt_alpha_cumprod**2).sqrt() * noise
+        )
+    
+
+    def p_losses(self, x_in, noise=None):
+        x_start = x_in['HR']    # High Resolution (good image) that gets diffused
+        [b, c, h, w] = x_start.shape
+        t = np.random.randint(1, self.num_timesteps + 1)
+        continuous_sqrt_alpha_cumprod = torch.FloatTensor(
+            np.random.uniform(
+                self.sqrt_alphas_cumprod_prev[t-1],
+                self.sqrt_alphas_cumprod_prev[t],
+                size=b
+            )
+        ).to(x_start.device)
+        continuous_sqrt_alpha_cumprod = continuous_sqrt_alpha_cumprod.view(
+            b, -1)
+        noise = default(noise, lambda: torch.randn_like(x_start))
+        x_noisy = self.q_sample(
+            x_start=x_start, continuous_sqrt_alpha_cumprod=continuous_sqrt_alpha_cumprod.view(-1, 1, 1, 1), noise=noise)
+        if not self.conditional:
+            x_recon = self.denoise_fn(x_noisy, continuous_sqrt_alpha_cumprod)
+        else:
+            y_in = x_in['SR'] # Low Resolution (crap image) that gets fixed and used to condition on...
+            concat_input = torch.cat([y_in,x_noisy], dim=1)
+            x_recon = self.denoise_fn(concat_input, continuous_sqrt_alpha_cumprod)
+            
+        loss = self.loss_func(noise, x_recon)  
+        return loss
+
+    
+    def forward(self, x, *args, **kwargs):
+        return self.p_losses(x, *args, **kwargs)
+
+
